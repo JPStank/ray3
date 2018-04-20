083f67a9bae0f7ce4a4d00c15e4b1fa34a8a3324
Joshua Stankiewicz <joshua.stankiewicz@intergraph.com>
2018-04-20 13:44:42 -0500

    integral

--- main.cpp
+++ main.cpp
@@ -9,26 +9,16 @@ int main(int argc, char* argv[])
 
 	int inside_circle = 0;
 	int inside_circle_stratified = 0;
-	int sqrt_N = 10000;
+	int N = 1000000;
+	float sum = 0;
 
-	for (int i = 0; i < sqrt_N; i++)
+	for (int i = 0; i < N; i++)
 	{
-		for (int j = 0; j < sqrt_N; j++)
-		{
-			float x = 2.0f * ((float)rand() / (float)RAND_MAX) - 1.0f;
-			float y = 2.0f * ((float)rand() / (float)RAND_MAX) - 1.0f;
-			if (x*x + y*y < 1.00f)
-				inside_circle++;
-
-			x = 2.0f * ((i + ((float)rand() / (float)RAND_MAX)) / sqrt_N) - 1.0f;
-			y = 2.0f * ((j + ((float)rand() / (float)RAND_MAX)) / sqrt_N) - 1.0f;
-			if (x*x + y*y < 1.0f)
-				inside_circle_stratified++;
-		}
+		float x = 2.0f * ((float)rand() / float(RAND_MAX));
+		sum += x*x;
 	}
 
+	std::cout << "I = " << 2 * sum / N << "\n";
 
-	std::cout << "Regular    Estimate of Pi = " << 4.0f * float(inside_circle) / (sqrt_N*sqrt_N) << "\n";
-	std::cout << "Stratified Estimate of Pi = " << 4.0f * float(inside_circle_stratified) / (sqrt_N*sqrt_N) << "\n";
 	return 0;
 }
